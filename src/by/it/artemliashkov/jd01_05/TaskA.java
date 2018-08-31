1d734af99a393c65c9168394e62c7af20bf3e2d3
aliashkov <art1699584@gmail.com>
2018-08-31 19:04:17 +0300

    Task jd01_05 (1-6). В задании Task B2 неверно подсчитан результат программы

--- src/by/it/artemliashkov/jd01_05/TaskA.java
+++ src/by/it/artemliashkov/jd01_05/TaskA.java
@@ -4,17 +4,36 @@ public class TaskA {
     public   static void main(String args[])
     {
         step1();
-        //step2();
-        //step3();
+        step2();
+        step3();
     }
 
     public static void step1()
     {
         double a=756.13;
         double x=0.3;
-        double z=Math.cos(Math.pow(x*x+Math.PI*6,5));
+        double z=Math.cos(Math.pow((x*x+Math.PI/6),5));
         z=z-Math.sqrt(x*Math.pow(a,3));
         z=z-Math.log(Math.abs((a-1.12*x)/4));
-        System.out.printf("При x=%f a=%f z=%f",x,a,z);
+        System.out.println("z="+z);
+    }
+
+    public static void step2()
+    {
+        double a=1.21;
+        double b=0.371;
+        double z=Math.tan(Math.pow(a+b,2))-Math.cbrt(a+1.5)+a*Math.pow(b,5)-(b/Math.log(a*a));
+        System.out.printf("При z=%f",z);
+    }
+
+    public static void step3()
+    {
+        double x=12.1;
+        for(double a=-5;a<=12;a=a+3.75)
+        {
+            double f=Math.exp(a*x)-3.45*a;
+            System.out.printf("При a=%.2f z=%g ",a,f);
+            System.out.println();
+        }
     }
 }
