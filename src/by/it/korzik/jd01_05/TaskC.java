f9c92e76238040fbe0041a728e2044a68a609fe3
KorzikD <korzikdmitriy@gmail.com>
2018-09-01 12:30:01 +0300

    jd01_01-jd01_05B complete

---
+++ src/by/it/korzik/jd01_05/TaskC.java
@@ -0,0 +1,26 @@
+package by.it.korzik.jd01_05;
+
+public class TaskC {
+    public static void main(String[] args) {
+        step1();
+    }
+
+    private static void step1() {
+        double[] arr = new double[30];
+        double iter = (9.0 - 5.33) / 30;
+        System.out.println("Массив A[]");
+        for (int v = 0; v < arr.length; v++) {
+            arr[v] =iter*v+5.33;
+            for (int i = 0; i < 5; i++) {
+            }
+        }
+        for (int v = 0; v < arr.length; v++) {
+                System.out.printf("A[% -3d] = %-9.5f", v, arr[v]);
+                int x=0;x++;
+                if (x==5){x=0;
+                    System.out.println();}
+        }
+        System.out.println();
+    }
+
+}
