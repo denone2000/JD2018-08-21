5d865085f2cbcacfcb7da68c3395e9ecb7501dbd
Галушка Игорь <galuropek@gmail.com>
2018-09-02 11:29:48 +0300

    jd01_06 Task A1

--- src/by/it/galushka/jd01_06/TaskA1.java
+++ src/by/it/galushka/jd01_06/TaskA1.java
@@ -8,17 +8,16 @@ import java.util.regex.Pattern;
  */
 public class TaskA1 {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(Poem.text);
-        sb.setCharAt(1, '#');
-        Pattern compile = Pattern.compile("[а-яА-яёЁ]{4,}");
-        Matcher matcher = compile.matcher(sb);
+        StringBuilder sBuilder = new StringBuilder(Poem.text);
+        Pattern compile = Pattern.compile("[А-Яа-яёЁ]{4,}");
+        Matcher matcher = compile.matcher(sBuilder);
         while (matcher.find()) {
             int position = matcher.start();
-            //System.out.println(matcher.group()+" "+position);
-            sb.setCharAt(position+3, '#');
-            if (matcher.end() - matcher.start() >= 7)
-                sb.setCharAt(position+6, '#');
+            sBuilder.setCharAt(position+3, '#');
+            if (matcher.end() - matcher.start() >= 7) {
+                sBuilder.setCharAt(position + 6, '#');
+            }
         }
-        System.out.println(sb);
+        System.out.println(sBuilder);
     }
 }
