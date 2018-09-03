bc63e969fd179cb561bd1d05cd3d4c51449652d5
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-09-03 10:59:12 +0300

    jd01_06 TaskB2

--- src/by/it/nesterovich/jd01_06/TaskB2.java
+++ src/by/it/nesterovich/jd01_06/TaskB2.java
@@ -1,6 +1,5 @@
 package by.it.nesterovich.jd01_06;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,8 +8,6 @@ public class TaskB2 {
     private static String[] sentence = {};
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(Poem.text);
-        //StringBuilder space = new StringBuilder(" ");
         Pattern patternEllipsis = Pattern.compile("\\.{3}");
         Matcher matcher = patternEllipsis.matcher(Poem.text);
         String sb2 = matcher.replaceAll(" ");
@@ -19,36 +16,30 @@ public class TaskB2 {
         Pattern patternSpace = Pattern.compile("\\s+");
         sb2 = patternSpace.matcher(sb2).replaceAll(" ");
         Pattern patternSplit = Pattern.compile("[.!]");
-
-
         sentence = patternSplit.split(sb2);
         for (int i = 0; i < sentence.length; i++) {
             sentence[i] = sentence[i].trim();
         }
-
         System.out.println();
         sortStringArray(sentence);
         printArrayString(sentence);
-
     }
 
     private static void sortStringArray(String[] sentence) {
         for (int i = 0; i < sentence.length; i++) {
-            for (int j = 0; j < sentence.length-1; j++) {
-                if (sentence[j].length()>sentence[j+1].length()){
+            for (int j = 0; j < sentence.length - 1; j++) {
+                if (sentence[j].length() > sentence[j + 1].length()) {
                     String temp = sentence[j];
-                    sentence[j] = sentence[j+1];
-                    sentence[j+1]=temp;
+                    sentence[j] = sentence[j + 1];
+                    sentence[j + 1] = temp;
                 }
             }
         }
     }
 
-
-    public static void printArrayString(String[] array) {
+    private static void printArrayString(String[] array) {
         for (String element : array) {
             System.out.println(element);
         }
     }
-
 }
