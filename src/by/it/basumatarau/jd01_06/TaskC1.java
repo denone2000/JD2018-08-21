ae3d81556ade4226ae04f80a7b766a9658ab106a
basumatarau <basumatarau@gmail.com>
2018-09-03 10:10:30 +0300

    jd01_06 TaskC1

--- src/by/it/basumatarau/jd01_06/TaskC1.java
+++ src/by/it/basumatarau/jd01_06/TaskC1.java
@@ -9,22 +9,43 @@ public class TaskC1 {
         String[] txt = Poem.text.split("\n");
         int max = txt[0].length();
         for (int i = 0; i < txt.length; i++) {
-            if (max>txt[i].length()){
+            if (max<txt[i].length()){
                 max = txt[i].length();
             }
         }
-        Pattern anyWord = Pattern.compile("");
+        Pattern anyWord = Pattern.compile("[а-яА-ЯёЁ,.:-]+");
         Matcher anyWordMatcher = anyWord.matcher("");
+        StringBuilder formattedText = new StringBuilder();
         for (int i = 0; i < txt.length; i++) {
             int delta = max-txt[i].length();
+
             anyWordMatcher.reset(txt[i]);
             int counter = 0;
-            while(anyWordMatcher.matches()){
+            while(anyWordMatcher.find()){
                 counter++;
             }
-            int addSpaces = txt[i].length()/counter;
-            int addMoreSpaces = txt[i].length()%counter;
+            int addSpaces = delta/(counter-1);
+            StringBuilder wordIndentRight = new StringBuilder();
+            for (int j = 0; j < addSpaces; j++) {
+                wordIndentRight.append(" ");
+            }
+            int addMoreSpaces = delta%(counter-1);
+            StringBuilder formattedString = new StringBuilder(txt[i]);
+            anyWordMatcher.reset(txt[i]);
+            int aux = 0;
 
+            for(int j=0; j < counter-1 && anyWordMatcher.find(); j++){
+                int pos = anyWordMatcher.end();
+                formattedString.insert(pos + aux +j*addSpaces, wordIndentRight);
+                if(addMoreSpaces-->0){
+                    formattedString.insert(pos+ aux++ + j*addSpaces, " ");
+                }
+            }
+
+            txt[i] = new String(formattedString);
+        }
+        for (int i = 0; i < txt.length; i++) {
+            System.out.println(txt[i]);
         }
     }
 
