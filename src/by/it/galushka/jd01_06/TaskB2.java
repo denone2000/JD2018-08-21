70f6b850c75b115d3096077e7d1e8cf24c3049c7
Галушка Игорь <galuropek@gmail.com>
2018-09-04 17:36:06 +0300

    jd01-06 Task B2 - done.

--- src/by/it/galushka/jd01_06/TaskB2.java
+++ src/by/it/galushka/jd01_06/TaskB2.java
@@ -1,57 +1,41 @@
 package by.it.galushka.jd01_06;
 
-/*TaskB2.Вывести через \nвсе предложения текста в порядке возрастания количества
-символов (!) в каждом из них. В предложениях нужно сначала заменить все небуквенные
-символы и их последовательности на один пробели выполнить trim() для каждого предложения.*/
-
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB2 {
 
-    private static String[] text = {};
-
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(Poem.text);
-        Pattern compile = Pattern.compile("[А-яЁё]+.[^\\.]+");
-        Matcher matcher = compile.matcher(sb);
-        while (matcher.find()) {
-            String group = matcher.group();
-            getArray(group);
+        StringBuilder poem = new StringBuilder(Poem.text);
+        Pattern delTripleDot = Pattern.compile("\\...");
+        String poemWithoutTripleDot = delTripleDot.matcher(poem).replaceAll("");
+        Pattern delEnter = Pattern.compile("\\n");
+        String poemWithoutEnter = delEnter.matcher(poemWithoutTripleDot).replaceAll(" ");
+        String[] array = poemWithoutEnter.split("\\.|\\!");
+        formatPoem(array);
+        sort(array);
+        printPoem(array);
+    }
+
+    private static void formatPoem(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i].replace(",", "").replace("-", "").replace(":", "");
+            StringBuilder findDoubleSpace = new StringBuilder(array[i]);
+            Pattern words = Pattern.compile("[А-яЁё]+");
+            Matcher matcher = words.matcher(findDoubleSpace);
+            String tmp = "";
+            while (matcher.find()) {
+                String group = matcher.group();
+                tmp = tmp + group + " ";
+            }
+            array[i] = tmp.trim();
         }
-        getString(text);
-        sort(text);
-        printGroup(text);
     }
 
-    private static void getArray(String group) {
-        text = Arrays.copyOf(text, text.length+1);
-        text[text.length-1] = group;
-    }
-
-    private static void printGroup(String[] text) {
+    private static void printPoem(String[] text) {
         for (String s : text) {
             System.out.println(s);
         }
-    }
-
-    private static void getString(String[] text) {
-        for (int i = 0; i < text.length; i++) {
-            String tmp = text[i];
-            text[i] = splitAndConcat(tmp);
-        }
-    }
-
-    public static String splitAndConcat(String tmp) {
-        String[] array = tmp.split("\\p{Punct}?\\s\\p{Punct}?\\s?");
-        String result = "";
-        for (int i = 0; i < array.length; i++) {
-            String trim = array[i].trim();
-            result = result.concat(trim + " ");
-        }
-        String trim = result.trim();
-        return trim;
     }
 
     public static void sort(String[] array) {
