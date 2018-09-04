576b1b50fce968fff8b83ce293ff6d74921b3e62
Галушка Игорь <galuropek@gmail.com>
2018-09-04 17:57:31 +0300

    jd01-06 Task C1 - done

--- src/by/it/galushka/jd01_06/TaskC1.java
+++ src/by/it/galushka/jd01_06/TaskC1.java
@@ -3,28 +3,42 @@ package by.it.galushka.jd01_06;
 import java.util.regex.Pattern;
 
 public class TaskC1 {
-    /*TaskC1. Отформатировать исходный текст с выравниванием по обоим краям. Для этого
-    добавить дополнительные пробелы между словами, так чтобы ширина строк стала равной, а
-    число пробелов между словами отличалось не более чем на единицу внутри каждой строки,
-    причем на единицу большие последовательности пробелов должны идти с начала строки.*/
 
     public static String[] array = {};
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(Poem.text);
+        StringBuilder poem = new StringBuilder(Poem.text);
         Pattern compile = Pattern.compile("\\n");
-        array = compile.split(sb);
+        array = compile.split(poem);
         int maxLength = findMaxLength(array);
-        align(array, maxLength);
-        printArray(array);
+        alignPoem(array, maxLength);
+        print(array);
     }
 
-    private static void printArray(String[] array) {
-        String text = "";
+    private static void alignPoem(String[] array, int maxLength) {
         for (int i = 0; i < array.length; i++) {
-            text = text + array[i] + "\n";
+            String[] splitArray = array[i].split(" ");
+            String str = " ";
+            if (array[i].length() < maxLength) {
+                while (array[i].length() < maxLength) {
+                    for (int j = 0; j < splitArray.length-1; j++) {
+                        splitArray[j] = splitArray[j].concat(str);
+                        array[i] = "";
+                        for (String elements : splitArray) {
+                            array[i] = array[i].concat(elements);
+                        }
+                        if(array[i].length() == maxLength)
+                            break;
+                    }
+                }
+            }
         }
-        System.out.println(text);
+    }
+
+    private static void print(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 
     private static int findMaxLength(String[] array) {
@@ -35,26 +49,5 @@ public class TaskC1 {
             }
         }
         return max;
-    }
-
-    private static void align(String[] array, int maxLength) {
-        for (int i = 0; i < array.length; i++) {
-            String[] tmp = array[i].split("\\p{Punct}?\\s");
-            StringBuilder sb;
-            String str = " ";
-            if (array[i].length() < maxLength) {
-                while (array[i].length() < maxLength) {
-                    for (int j = 0; j < tmp.length-1; j++) {
-                        tmp[j] = tmp[j].concat(str);
-                        array[i] = "";
-                        for (String elements : tmp) {
-                            array[i] = array[i].concat(elements);
-                        }
-                        if(array[i].length() == maxLength)
-                            break;
-                    }
-                }
-            }
-        }
     }
 }
