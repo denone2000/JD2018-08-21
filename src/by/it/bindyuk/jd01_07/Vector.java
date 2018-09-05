22e3578d5a8ecc0085b9c154a1a24f94ab29ec21
EvgeniyBindyuk <evganius@gmail.com>
2018-09-05 14:23:16 +0300

    B

---
+++ src/by/it/bindyuk/jd01_07/Vector.java
@@ -0,0 +1,77 @@
+package by.it.bindyuk.jd01_07;
+
+public class Vector extends AbstractVar {
+    private double[] value;
+
+    public Vector(double[] value) {
+        this.value = value;
+    }
+
+    public Vector(Vector vector) {
+        value = vector.value;
+    }
+
+    public Vector(String strVector){
+      strVector.replace('{', ' ').replace('}',' ').trim();
+      String [] strArray = strVector.split(",");
+      value = new double[strArray.length];
+        for (int i = 0; i <strArray.length ; i++) {
+            value[i] = Double.parseDouble(strArray[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("{");
+        String delimeter = "";
+        for (double currentValue : value) {
+            result.append(delimeter).append(currentValue);
+            delimeter = ", ";
+        }
+        result.append("}");
+        return result.toString();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public Vector(String strVector) {
+//        strVector.replace('{', ' ').replace('}', ' ').trim();
+//        String[] strArray = strVector.split(",\\s*");
+//        value = new double[strArray.length];
+//        for (int i = 0; i <strArray.length ; i++) {
+//            value[i] = Double.parseDouble(strArray[i]);
+//        }
+//
+//    }
+//
+//    @Override
+//    public  String toString(){
+//        StringBuilder result = new StringBuilder("{");
+//        String delimeter = "";
+//        for (double currentValue : value) {
+//            result.append(delimeter).append(currentValue);
+//            delimeter = ", ";
+//        }
+//        result.append("}");
+//        return result.toString();
+//}
+//}
