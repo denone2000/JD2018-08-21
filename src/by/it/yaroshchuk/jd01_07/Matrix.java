677a57619a35364ac2094fcd704fd19f76e4ace2
yaroshchuk <5.kevr.5@gmail.com>
2018-09-06 18:04:30 +0300

    jd01_07 TaskC3 yaroshchuk

--- src/by/it/yaroshchuk/jd01_07/Matrix.java
+++ src/by/it/yaroshchuk/jd01_07/Matrix.java
@@ -16,46 +16,23 @@ class Matrix extends  AbstractVar {
     }
 
     Matrix (String str){
-        /*Pattern pattern = Pattern.compile("(},)*");
-        String[] strArray = pattern.split(str);
-        pattern = Pattern.compile("([0-9]*[.])?[0-9]+");
+        int occurrencesCount = str.length() - str.replace("}","").length() - 1;
+        //String regex = "([0-9]*[.])?[0-9]+";
+        Pattern pattern = Pattern.compile("([0-9]*[.])?[0-9]+");
         StringBuilder sb = new StringBuilder();
-        int length = 0;
-        for (int i = 0; i < strArray.length; i++) {
-            Matcher matcher = pattern.matcher(strArray[i]);
-            while(matcher.find()){
-                sb.append(matcher.group());
-                sb.append(" ");
-                length++;
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            sb.append(matcher.group());
+            sb.append(" ");
+        }
+        String[] strValue =  sb.toString().split(" ");
+        int colCount = strValue.length / occurrencesCount;
+        value = new double[occurrencesCount][colCount];
+        for (int i = 0, k = 0; i < value.length; i++) {
+            for (int j = 0; j < value[0].length; j++, k++) {
+                value[i][j] = Double.parseDouble(strValue[k]);
             }
         }
-        String [][] strValueArray*/
-        /*String regex = "([0-9]*[.])?[0-9]+";
-        Pattern pattern = Pattern.compile(regex);
-        String strArray[] = str.split("},");
-        StringBuilder sb = new StringBuilder();
-        String[] strBuffer = new String[];
-        int lengthValue = 0;
-        for (int i = 0; i < strArray.length; i++) {
-
-            Matcher matcher = pattern.matcher(strArray[i]);
-            while (matcher.find()) {
-                sb.append(matcher.group());
-                sb.append(" ");
-            }
-            String strSb = new String(sb);
-            String[] strBuffer = strSb.trim().split(" ");
-            String[][] strValueArray = new String [strArray.length][strBuffer.length];
-            lengthValue = strBuffer.length;
-            for (int j = 0; j < strBuffer.length; j++) {
-                strValueArray[i][j] = strBuffer[i];
-            }
-            sb.delete(0, sb.length()-1);
-        }
-        value = new double[strArray.length] [lengthValue];
-        for (int j = 0; j < lengthValue; j++) {
-            value[i][j] = Double.parseDouble(strBuffer[i]);
-        }*/
     }
 
     @Override
--- src/by/it/yaroshchuk/jd01_08/Matrix.java
+++ src/by/it/yaroshchuk/jd01_08/Matrix.java
@@ -13,46 +13,27 @@ class Matrix extends Var {
     }
 
     Matrix (String str){
-        /*Pattern pattern = Pattern.compile("(},)*");
-        String[] strArray = pattern.split(str);
-        pattern = Pattern.compile("([0-9]*[.])?[0-9]+");
-        StringBuilder sb = new StringBuilder();
-        int length = 0;
-        for (int i = 0; i < strArray.length; i++) {
-            Matcher matcher = pattern.matcher(strArray[i]);
-            while(matcher.find()){
-                sb.append(matcher.group());
-                sb.append(" ");
-                length++;
-            }
-        }
-        String [][] strValueArray*/
-        /*String regex = "([0-9]*[.])?[0-9]+";
-        Pattern pattern = Pattern.compile(regex);
-        String strArray[] = str.split("},");
-        StringBuilder sb = new StringBuilder();
-        String[] strBuffer = new String[];
-        int lengthValue = 0;
-        for (int i = 0; i < strArray.length; i++) {
 
-            Matcher matcher = pattern.matcher(strArray[i]);
-            while (matcher.find()) {
-                sb.append(matcher.group());
-                sb.append(" ");
-            }
-            String strSb = new String(sb);
-            String[] strBuffer = strSb.trim().split(" ");
-            String[][] strValueArray = new String [strArray.length][strBuffer.length];
-            lengthValue = strBuffer.length;
-            for (int j = 0; j < strBuffer.length; j++) {
-                strValueArray[i][j] = strBuffer[i];
-            }
-            sb.delete(0, sb.length()-1);
-        }
-        value = new double[strArray.length] [lengthValue];
-        for (int j = 0; j < lengthValue; j++) {
-            value[i][j] = Double.parseDouble(strBuffer[i]);
-        }*/
+    }
+
+    @Override
+    public Var add(Var other) {
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        return super.div(other);
     }
 
     @Override
