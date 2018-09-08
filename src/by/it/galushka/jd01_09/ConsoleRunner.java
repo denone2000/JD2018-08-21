e0e3a99e1d152cfc8cb9609b9c43a2b49542afa2
Галушка Игорь <galuropek@gmail.com>
2018-09-08 21:10:16 +0300

    jd01_09

---
+++ src/by/it/galushka/jd01_09/ConsoleRunner.java
@@ -0,0 +1,11 @@
+package by.it.galushka.jd01_09;
+
+import javax.swing.text.html.parser.Parser;
+import java.util.Scanner;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+    }
+}
