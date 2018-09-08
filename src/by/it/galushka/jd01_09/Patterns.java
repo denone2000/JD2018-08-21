e43932d121c9ab8dba050165830972a80b5c5baa
Галушка Игорь <galuropek@gmail.com>
2018-09-08 23:32:09 +0300

    jd01_09 A

---
+++ src/by/it/galushka/jd01_09/Patterns.java
@@ -0,0 +1,9 @@
+package by.it.galushka.jd01_09;
+
+import java.util.regex.Pattern;
+
+public class Patterns {
+
+    static final String OPERATORS = "[-+*/]";
+    static final String SKALAR = "-?\\d+(\\.\\d+)?";
+}
--- src/by/it/galushka/jd01_09/Var.java
+++ src/by/it/galushka/jd01_09/Var.java
@@ -2,6 +2,15 @@ package by.it.galushka.jd01_09;
 
 abstract class Var implements Operation {
 
+    static Var createVar(String inputLine) {
+        inputLine = inputLine.trim();
+        if (inputLine.matches(Patterns.SKALAR)) {
+            return new Scalar(inputLine);
+        }
+        else
+            return null;
+    }
+
     @Override
     public Var add(Var other) {
         System.out.println("Операция сложения "+this+" + "+other+" невозможна!");
