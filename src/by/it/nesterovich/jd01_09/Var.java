c56cab5197decd96e4ce0757664958590a45068b
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-09-08 20:51:38 +0300

    jd01_09 Start

---
+++ src/by/it/nesterovich/jd01_09/Var.java
@@ -0,0 +1,49 @@
+package by.it.nesterovich.jd01_09;
+
+abstract class Var implements Operation {
+
+    static Var createVar(String operand){
+        operand = operand.trim().replace("\\s+","");
+        if (operand.matches(Patterns.SCALAR)){
+            new Scalar(operand);
+        }
+        if (operand.matches(Patterns.VECTOR)){
+            new Vector(operand);
+        }
+        if (operand.matches(Patterns.MATRIX)){
+            new Matrix(operand);
+        }
+        //TODO add exception create vars
+        return null;
+    }
+
+    @Override
+    public Var add(Var other) {
+        System.out.println("Операция сложения " + this + "+" + other + " невозможна");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Операция вычитания " + this + "-" + other + " невозможна");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Операция умножения " + this + "*" + other + " невозможна");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Операция деления " + this + "/" + other + " невозможна");
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Это класс Var";
+    }
+}
