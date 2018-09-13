2a337636cb8d5fa655c24927c190567d6f9649ac
Khmelov <375336849110@tut.by>
2018-09-13 12:23:11 +0300

    JD01_11 TaskA

---
+++ src/by/it/_tasks_/jd01_11/Runner.java
@@ -0,0 +1,20 @@
+package by.it._tasks_.jd01_11;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+
+
+        Integer[] m= (Integer[]) new Object[]{};
+        System.out.println(Arrays.toString(m));
+
+        List<String> list=new ListA<>();
+        list.add("TWO");
+        list.add("THREE");
+        System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
+    }
+}
