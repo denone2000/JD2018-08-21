52dc24210413523c8458eff84effbe29e37cb5e4
Khmelov <375336849110@tut.by>
2018-09-13 16:46:48 +0300

    lesson 11

--- src/by/it/akhmelev/jd01_11/Runner.java
+++ src/by/it/akhmelev/jd01_11/Runner.java
@@ -5,11 +5,6 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-
-
-        Integer[] m= (Integer[]) new Object[]{};
-        System.out.println(Arrays.toString(m));
-
         List<String> list=new ListA<>();
         list.add("ONE");
         list.add("TWO");
