9a0319a0a1204dbb91d33ae6715191bdcf2d872c
Basumatarau <basumatarau@gmail.com>
2018-09-16 00:24:32 +0300

    jd01_12. start...

--- src/by/it/basumatarau/jd01_12/TaskA2.java
+++ src/by/it/basumatarau/jd01_12/TaskA2.java
@@ -1,17 +1,22 @@
 package by.it.basumatarau.jd01_12;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class TaskA2 {
-    Set<Integer> getUnion(Set<Integer> a, Set<Integer> b){
+    static Set<Integer> getUnion(Set<Integer> a, Set<Integer> b){
         Set<Integer> result = new HashSet<>(a);
         result.addAll(b);
         return result;
     }
-
+    static Set<Integer> getCross(Set<Integer> a, Set<Integer> b){
+        Set<Integer> result = new HashSet<>(a);
+        result.retainAll(b);
+        return result;
+    }
     public static void main(String[] args) {
-
+        Set<Integer> someIntSet = new HashSet<>(Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6}));
+        Set<Integer> someOtherIntSet = new HashSet<>(Arrays.asList(new Integer[]{4, 5, 6, 7, 8, 9}));
+        System.out.println(TaskA2.getCross(someIntSet, someOtherIntSet));
+        System.out.println(TaskA2.getUnion(someIntSet, someOtherIntSet));
     }
 }
