021435d80fe938e6deef9c801a440e08c67aaefa
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2018-09-17 22:34:15 +0300

    Задания к занятию 12 A, B1

--- src/by/it/korolchuk/jd01_12/TaskB1.java
+++ src/by/it/korolchuk/jd01_12/TaskB1.java
@@ -1,4 +1,22 @@
 package by.it.korolchuk.jd01_12;
 
+import java.util.*;
+
 public class TaskB1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word;
+
+        List<String> words = new ArrayList<>();
+
+       while (!(word = sc.next()).equals("end")){
+           words.add(word.replace("[\\.:,;!?/—]", " "));
+       }
+        System.out.println(words);
+
+        Set<String> uniqueWords = new HashSet<String>(words);
+        for (String w : uniqueWords) {
+            System.out.println(w + "=" + Collections.frequency(words, w));
+        }
+    }
 }
