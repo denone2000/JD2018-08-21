70428e93a99dfa2d8ccadb4d5b8c2f3cc087a785
Basumatarau <basumatarau@gmail.com>
2018-09-17 18:20:49 +0300

    jd01_12. C3 ...touch-ups

--- src/by/it/basumatarau/jd01_12/TaskC3.java
+++ src/by/it/basumatarau/jd01_12/TaskC3.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class TaskC3 {
     public static void main(String[] args) {
         String testLineFix = "{((2+3)+([3*4]+4))}";
-        String testLineFix2 = "{3}*({2}*([1]-2+3)+([3*4]+4))";
+        String testLineFix2 = "[";
         String testLineFailed = "{3}*({2}*[1-2+3))";
 
         System.out.println(parenthOrderChecker(testLineFix)+ testLineFix);
@@ -30,6 +30,7 @@ public class TaskC3 {
                 separatorStack.push(token);
             }else if(token.matches("[)}\\]]")){
                 buffer = token;
+                if(separatorStack.empty()) return false;
                 //noinspection LoopStatementThatDoesntLoop the loop statement will be likely necessary for further work with the code (no latency footprint is anticipated)
                 while(!((token=separatorStack.pop()).contains("[({\\[]"))){
                     if(!token.concat(buffer).matches("(\\[])|(\\{})|(\\(\\))")){
