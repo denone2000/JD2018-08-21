2638cec3bebffc726101cff1e2602dfc29a72a2a
Khmelov <375336849110@tut.by>
2018-09-18 12:37:50 +0300

    JD01_12 TaskB1 + calc

--- src/by/it/akhmelev/jd01_13/TaskA.java
+++ src/by/it/akhmelev/jd01_13/TaskA.java
@@ -11,18 +11,17 @@ public class TaskA {
             else
                 Integer.parseInt("привет");
         } catch (NullPointerException | NumberFormatException e) {
-            e.printStackTrace();
-//            String myClassName = TaskA.class.getName();
-//            String name = e.getClass().getName();
-//            StackTraceElement[] stackTrace = e.getStackTrace();
-//            for (StackTraceElement element : stackTrace) {
-//                if (element.getClassName().equals(myClassName)) {
-//                    System.out.println(" name: " + name);
-//                    System.out.println("class: " + element.getClassName());
-//                    System.out.println(" line: " + element.getLineNumber());
-//                    break;
-//                }
-//            }
+            String myClassName = TaskA.class.getName();
+            String name = e.getClass().getName();
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement element : stackTrace) {
+                if (element.getClassName().equals(myClassName)) {
+                    System.out.println(" name: " + name);
+                    System.out.println("class: " + element.getClassName());
+                    System.out.println(" line: " + element.getLineNumber());
+                    break;
+                }
+            }
         }
 
     }
