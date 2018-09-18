eaee4b82856bf8f5433c61a5e1b9cccddd181d8e
Галушка Игорь <galuropek@gmail.com>
2018-09-18 17:19:04 +0300

    jd01_13 TaskA - done

--- src/by/it/galushka/jd01_13/TaskA.java
+++ src/by/it/galushka/jd01_13/TaskA.java
@@ -1,5 +1,27 @@
 package by.it.galushka.jd01_13;
 
+import java.util.HashMap;
+
 public class TaskA {
-    
+
+    public static void main(String[] args) {
+        try {
+        if (Math.random() > 0.5)
+            new HashMap<String, String>(null);
+        else
+            Integer.parseInt("привет");
+        }
+        catch (Exception e) {
+            String myClassName = e.getClass().getName();
+            StackTraceElement[] elements = e.getStackTrace();
+            for (StackTraceElement element : elements) {
+                if (TaskA.class.getName().equals(element.getClassName())) {
+                    System.out.println(" name: " + myClassName);
+                    System.out.println("class: " + element.getClassName());
+                    System.out.println(" line: " + element.getLineNumber());
+                    break;
+                }
+            }
+        }
+    }
 }
