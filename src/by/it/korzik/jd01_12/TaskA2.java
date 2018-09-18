e89a66575a3db0a3ae56155100b53643f2d45366
KorzikD <korzikdmitriy@gmail.com>
2018-09-18 11:17:47 +0300

    jd_01_12

--- src/by/it/korzik/jd01_12/TaskA2.java
+++ src/by/it/korzik/jd01_12/TaskA2.java
@@ -9,21 +9,29 @@ import java.util.Set;
 
 public class TaskA2 {
     public static void main(String[] args) {
-        Integer[] first={1,2,3,4,4,4,4,4,4,444,5};
-        Integer[] second={1,2,3,5555,6,7,83,6};
+        Integer[] first = {1, 2, 3, 4, 4, 4, 4, 4, 4, 444, 5};
+        Integer[] second = {1, 2, 3, 5555, 6, 7, 83, 6};
 
-        List<Integer> listFirst=Arrays.asList(first);
-        List<Integer> listSecond=Arrays.asList(second);
+        List<Integer> listFirst = Arrays.asList(first);
+        List<Integer> listSecond = Arrays.asList(second);
 
-        Set<Integer> setFirst=new HashSet<>(listFirst);
+        Set<Integer> setFirst = new HashSet<>(listFirst);
         HashSet<Integer> setSecond = new HashSet<>(listSecond);
 
         TaskA2 task = new TaskA2();
-        task.getUnion(setFirst,setSecond);
+        getUnion(setFirst, setSecond);
+        getCross(setFirst,setSecond);
     }
-    Set<Integer> getUnion(Set<Integer> first,Set<Integer> second){
-        Set<Integer> result=new HashSet<>(second);
-        result.addAll(first);
+
+    static Set<Integer> getUnion(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> result = new HashSet<>(first);
+        result.addAll(second);
+        return result;
+    }
+
+    static Set<Integer> getCross(Set<Integer> first, Set<Integer> second) {
+        Set<Integer> result = new HashSet<>(first);
+        result.retainAll(second);
         return result;
     }
 }
