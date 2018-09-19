f3b2ec2d518882aa2c726923ef56612153ebad41
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-09-19 10:55:38 +0300

    jd01_13 TaskC

--- src/by/it/nesterovich/jd01_13/TaskC.java
+++ src/by/it/nesterovich/jd01_13/TaskC.java
@@ -1,44 +1,41 @@
 package by.it.nesterovich.jd01_13;
 
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class TaskC {
 
     private static int count = 0;
-    private static String[] numbers;
     private static ArrayList<Double> strings = new ArrayList<>();
+    private static Scanner scanner;
 
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        String text;
-
-        while (!(text = scanner.next()).equalsIgnoreCase("END")) {
-            String[] str =text.split("\\n");
-            for (String element : str) {
-                readData(element);
-            }
-
+        scanner = new Scanner(System.in);
+        while (count <= 5) {
+            readData();
         }
     }
 
-    static void readData(String str) throws Exception {
-
+    private static void readData() throws Exception {
+        String text = scanner.next();
+        String[] numbers = text.split("\\n");
         try {
-            Double num =Double.parseDouble(str);
-            strings.add(num);
-
+            for (String number : numbers) {
+                Double num = Double.parseDouble(number);
+                if (count < 5) {
+                    strings.add(num);
+                } else {
+                    count++;
+                    throw new Exception();
+                }
+            }
         } catch (Exception e) {
             Thread.sleep(100);
-            for (int i = strings.size()-1; i >=0 ; i--) {
+            for (int i = strings.size() - 1; i >= 0; i--) {
                 System.out.print(strings.get(i) + " ");
             }
             System.out.println();
             count++;
-            if (count==5) {
-                throw new Exception();
-            }
         }
     }
 }
