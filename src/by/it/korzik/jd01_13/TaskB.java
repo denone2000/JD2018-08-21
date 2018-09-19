e61ada0fc481ecfbd3b3477430502766d689aed1
KorzikD <korzikdmitriy@gmail.com>
2018-09-19 16:28:44 +0300

    jd_01_13

--- src/by/it/korzik/jd01_13/TaskB.java
+++ src/by/it/korzik/jd01_13/TaskB.java
@@ -1,17 +1,25 @@
 package by.it.korzik.jd01_13;
 
-import java.util.HashMap;
 
-public class TaskA {
+import java.util.Scanner;
+
+public class TaskB {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String line;
+        double num;
+        double sqrt;
+        double sum=0;
         try {
-            if (Math.random() > 0.5)
-                new HashMap<String, String>(null);
-            else
-                Integer.parseInt("привет");
+            while (!(line=scan.next()).equalsIgnoreCase("end")) {
+            num=Double.parseDouble(line);
+            sum+=num;
+            sqrt=Math.sqrt(sum);
+                System.out.println(sqrt);
+            }
         }
-        catch (NullPointerException| NumberFormatException e){
-            String myTaskName = TaskA.class.getName();
+        catch (ArithmeticException | NumberFormatException e){
+            String myTaskName = TaskB.class.getName();
             String name = e.getClass().getName();
             StackTraceElement[] stackTrace = e.getStackTrace();
             for (StackTraceElement element : stackTrace) {
