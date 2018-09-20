ca84bc5359d03059c6f5a235337b06869dd25fc2
Khmelov <375336849110@tut.by>
2018-09-20 12:31:08 +0300

    JD01_14 TaskC Sample

---
+++ src/by/it/akhmelev/jd01_13/TaskC.java
@@ -0,0 +1,47 @@
+package by.it.akhmelev.jd01_13;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TaskC {
+
+    static Scanner scanner;
+    static int counterError=0;
+    static List<Double> list = new ArrayList<Double>(){
+        @Override
+        public String toString() {
+            StringBuilder sb=new StringBuilder();
+            for (int i = this.size()-1; i >=0 ; i--) {
+                sb.append(this.get(i)).append(' ');
+            }
+            return sb.toString();
+        }
+    };
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        while (true) {
+            readData();
+        }
+    }
+
+    private static void readData() {
+        String str = scanner.next();
+        try {
+            double v = Double.parseDouble(str);
+            list.add(v);
+        } catch (NumberFormatException e) {
+            try {
+                counterError++;
+                if (counterError>5)
+                    throw e;
+                Thread.sleep(100);
+                System.out.println(list);
+
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+}
