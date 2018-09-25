fe463cb269d1ad4476c76779d31f1452f4dac829
Edward <torresthebes@mail.ru>
2018-09-25 21:35:33 +0300

    jd01_14 (B-C)

--- src/by/it/litvin/jd01_15/TaskA.java
+++ src/by/it/litvin/jd01_15/TaskA.java
@@ -1,60 +1,65 @@
 package by.it.litvin.jd01_15;
 
 
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskA {
     public static void main(String[] args) throws IOException {
-        String root=System.getProperty("user.dir");
-        String path=root+"/src/by/it/litvin/jd01_15/";
-        String filename=path+"matrix.txt";
+        String root = System.getProperty("user.dir");
+        String path = root + "/src/by/it/litvin/jd01_15/";
+        String filename = path + "matrix.txt";
         System.out.println(filename);
-        int[][] matrix=new int[6][4];
+
+        int[][] matrix = new int[6][4];
         for (int[] row : matrix) {
-            for (int i = 0; i < row.length; i++) {
-                row[i]=(int)(Math.random()*31-15);
+            for (int j = 0; j < row.length; j++) {
+                row[j] = (int) (Math.random() * 31 - 15);
 
             }
 
         }
-        try (
-            FileWriter writer=new FileWriter(filename);
-            PrintWriter printWriter=new PrintWriter(writer);
-        ){
+        List<String> list = new ArrayList<>();
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename))
+        ) {
             for (int[] row : matrix) {
                 for (int value : row) {
-                    printWriter.printf("%3d ", value);
+                    writer.printf("%3d ", value);
+
+            }
+            writer.println();
+        }}
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+
+        try (BufferedReader reader=new BufferedReader(new FileReader(filename))
+        ) {
+            while (reader.ready()) {
+                list.add(reader.readLine());
+            }
+            int [][] matrixConsole = new int[list.size()][0];
+            for (int i = 0; i < list.size(); i++) {
+                String str = list.get(i);
+                String[] strMas = str.trim().split("\\s+");
+                matrixConsole[i] = new int[strMas.length];
+                for (int j = 0; j < strMas.length; j++) {
+                    matrixConsole[i][j] = Integer.parseInt(strMas[j]);
+
                 }
-                    System.out.println(matrix);
+            }
+            for (int[] row : matrix) {
+                for (int value : row) {
+                    System.out.printf("%3d ", value);
                 }
-                printWriter.println();
+                System.out.println();
             }
 
-        try (
-            FileReader fileReader = new FileReader(filename);
-            BufferedReader reader = new BufferedReader(fileReader);
-            ){
-            List<String> rows=new ArrayList<>();
-            while (reader.ready()) {
-                rows.add(reader.readLine());
-            }
-            matrix=new int[rows.size()][0];
-            int rowCounter=0;
-            for (String str : rows) {
-                String [] strArr=str.trim().split("\\s+");
-                matrix[rowCounter]=new int[strArr.length];
-                for (int i = 0; i < strArr.length; i++) {
-                    matrix[rowCounter][i] = Integer.parseInt(strArr[i]);
-                }}
-                for (int[] row : matrix) {
-                    for (int value : row) {
-                        System.out.printf("%3d ", value);
-                    }
-                    System.out.println(matrix);
-                }
-            System.out.println();
-            }
-}}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
