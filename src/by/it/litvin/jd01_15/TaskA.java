43ac9b82083a963383ac5a200a8f9a098f2c51f5
Edward <torresthebes@mail.ru>
2018-09-25 18:34:32 +0300

    jd01_15 A

--- src/by/it/litvin/jd01_15/TaskA.java
+++ src/by/it/litvin/jd01_15/TaskA.java
@@ -1,15 +1,15 @@
 package by.it.litvin.jd01_15;
 
-import by.it.galushka.jd01_03.Print;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskA {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String root=System.getProperty("user.dir");
-        String path=root+"/src/by/it/_tasks_/jd01_15/";
+        String path=root+"/src/by/it/litvin/jd01_15/";
         String filename=path+"matrix.txt";
         System.out.println(filename);
         int[][] matrix=new int[6][4];
@@ -20,18 +20,41 @@ public class TaskA {
             }
 
         }
-        try {
+        try (
             FileWriter writer=new FileWriter(filename);
             PrintWriter printWriter=new PrintWriter(writer);
+        ){
             for (int[] row : matrix) {
                 for (int value : row) {
-                    printWriter.printf("%3d ",value);
+                    printWriter.printf("%3d ", value);
+                }
+                    System.out.println(matrix);
                 }
                 printWriter.println();
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+        try (
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader reader = new BufferedReader(fileReader);
+            ){
+            List<String> rows=new ArrayList<>();
+            while (reader.ready()) {
+                rows.add(reader.readLine());
+            }
+            matrix=new int[rows.size()][0];
+            int rowCounter=0;
+            for (String str : rows) {
+                String [] strArr=str.trim().split("\\s+");
+                matrix[rowCounter]=new int[strArr.length];
+                for (int i = 0; i < strArr.length; i++) {
+                    matrix[rowCounter][i] = Integer.parseInt(strArr[i]);
+                }}
+                for (int[] row : matrix) {
+                    for (int value : row) {
+                        System.out.printf("%3d ", value);
+                    }
+                    System.out.println(matrix);
+                }
+            System.out.println();
+            }
+}}
