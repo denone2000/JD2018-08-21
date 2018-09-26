416c8411471dfa300e8238c0ae40bc86ef01b615
Khmelov <375336849110@tut.by>
2018-09-26 19:27:04 +0300

    lesson 02_01

--- src/by/it/akhmelev/jd02_01/Dispathcer.java
+++ src/by/it/akhmelev/jd02_01/Dispathcer.java
@@ -3,18 +3,18 @@ package by.it.akhmelev.jd02_01;
 class Dispathcer {
     static final int KSPEED = 100;
 
-    private static volatile int buyesInMarket = 0;
+    private static volatile int buyersInMarket = 0;
 
     static void addBuyer() {
-        buyesInMarket++;
+        buyersInMarket++;
     }
 
     static void removeBuyer() {
-        buyesInMarket--;
+        buyersInMarket--;
     }
 
-    static int getBuyesInMarket() {
-        return buyesInMarket;
+    static int getBuyersInMarket() {
+        return buyersInMarket;
     }
 
 
