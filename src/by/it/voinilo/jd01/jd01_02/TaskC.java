3fa2f618cf9e93f712bc6949826173496b110068
VoiniloDmitriy <workmail1421@gmail.com>
2018-09-27 00:56:29 +0300

    jd01_14(A)

---
+++ src/by/it/voinilo/jd01/jd01_02/TaskC.java
@@ -0,0 +1,28 @@
+package by.it.voinilo.jd01.jd01_02;
+
+import java.util.Scanner;
+
+public class TaskC {
+
+    public static void main (String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        step1(n);
+
+    }
+
+    public static void step1(int n){
+
+        for (int i = 1; i <= n ; i++) {
+
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ src/by/it/voinilo/jd01/jd01_10/Bean.java
@@ -0,0 +1,13 @@
+package by.it.voinilo.jd01.jd01_10;
+
+public class Bean {
+@Param(a=3, b=4)
+    double sum (int a, int b){return  a+b;}
+    @Param(a=30, b=4)
+    double min (int a, int b){return  a<b ? a:b;}
+    @Param(a=32, b=4)
+    static double max (int a, int b){return  a>b ? a:b;}
+
+    static double avg (int a, int b){return  (a+b)/2.0;}
+
+}
