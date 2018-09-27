aba22acdc7ce6c3967887a452e56f643ef059032
Александр Хмелев <Khmelov@users.noreply.github.com>
2018-09-27 09:30:46 +0300

    fix

--- src/by/it/litvin/jd01_14/TaskA.java
+++ src/by/it/litvin/jd01_14/TaskA.java
@@ -7,7 +7,7 @@ public class TaskA {
 
     private static String dir(Class<?>cl){
         String path= System.getProperty("user.dir")+ File.separator+"src"+File.separator;
-        String clDir=cl.getName().replace(cl.getSimpleName()," ").replace(".",File.separator);
+        String clDir=cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
         return path+clDir;
     }
     public static void main(String[] args) {
