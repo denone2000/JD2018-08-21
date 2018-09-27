26675394861b782c71c089273c5edbba3a7e41f2
Khmelov <375336849110@tut.by>
2018-09-27 12:23:06 +0300

    JD02_02 Level A

---
+++ src/by/it/akhmelev/jd02_02/Dispathcer.java
@@ -0,0 +1,33 @@
+package by.it.akhmelev.jd02_02;
+
+class Dispathcer {
+    static final int KSPEED = 100;
+
+
+    private static volatile int buyesInMarket = 0;
+    private static volatile int factCount = 0;
+    private static final int PLAN_COUNT = 100;
+
+    static synchronized void addBuyer() {
+        buyesInMarket++;
+    }
+
+    static synchronized void buyerLeaveMarket() {
+        buyesInMarket--;
+        factCount++;
+    }
+
+    static synchronized int getBuyesInMarket() {
+        return buyesInMarket;
+    }
+
+    static synchronized boolean marketIsOpened() {
+        return (buyesInMarket + factCount) < PLAN_COUNT;
+    }
+
+    static synchronized boolean planComplete() {
+        return factCount >= PLAN_COUNT;
+    }
+
+
+}
