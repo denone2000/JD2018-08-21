0c24f5dcbf0d4d2bce04ede771b9a7ba2d733817
KorzikD <korzikdmitriy@gmail.com>
2018-09-28 17:30:05 +0300

    jd02_01

---
+++ src/by/it/korzik/jd02_02/Buyer.java
@@ -0,0 +1,73 @@
+package by.it.korzik.jd02_02;
+
+import java.util.Map;
+import java.util.Set;
+
+import static by.it.korzik.jd02_01.Market.goodsMap;
+
+public class Buyer extends Thread implements IBuyer, IUseBacket {
+
+    Buyer(int num) {
+        super("Покупатель" + num);
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this.getName() + " вошел в магазин");
+    }
+
+    @Override
+    public void takeBacket() {
+        int random = Util.random(100, 200);
+        Util.sleep(random);
+        System.out.println(this.getName() + " взял корзину");
+    }
+
+
+    @Override
+    public void chooseGoods() {
+        int random1 = Util.random(1, 4);
+        for (int i = 0; i < random1; i++) {
+            int random = Util.random(500, 2000);
+            Util.sleep(random);
+            System.out.println(this.getName() + " выбирает товар");
+            putGoodsToBacket();
+        }
+    }
+
+    @Override
+    public void putGoodsToBacket() {
+        int random = Util.random(100, 200);
+        Util.sleep(random);
+        String goods = choose();
+        System.out.println(this.getName() + " положил " + goods + " в корзину");
+    }
+
+    private String choose() {
+        int count = 0;
+        int size = (goodsMap.size() - 1);
+        int choose = Util.random(size);
+        Set<Map.Entry<String, Integer>> entries = goodsMap.entrySet();
+        for (Map.Entry<String, Integer> next : entries) {
+            if (count != choose) {
+                count++;
+            } else {
+                return next.getKey();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(this.getName() + " вышел из магазина");
+    }
+
+    @Override
+    public void run() {
+        enterToMarket();
+        takeBacket();
+        chooseGoods();
+        goOut();
+    }
+}
---
+++ src/by/it/korzik/jd02_02/Cashier.java
@@ -0,0 +1,13 @@
+package by.it.korzik.jd02_02;
+
+public class Cashier implements Runnable {
+
+    Cashier(int num){
+        super("Кассир№"+num);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
