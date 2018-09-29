2050032f5c6b88170416e5792cc762ec134a11d8
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-09-29 18:00:06 +0300

    jd02_03 copy-paste

---
+++ src/by/it/nesterovich/jd02_03/Cashier.java
@@ -0,0 +1,38 @@
+package by.it.nesterovich.jd02_03;
+
+class Cashier implements Runnable {
+
+    private int number;
+
+    Cashier(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this + " открыл кассу");
+        while (!Dispatcher.planComplete()) {
+            Buyer buyer = QueueBuyers.pollBuyer();
+            if (buyer != null) {
+                System.out.println(this + " начал обслуживание:  " + buyer);
+                int timeout = Util.random(2000, 5000);
+                Util.sleep(timeout);
+                System.out.println(this + " завершил обслуживание:  " + buyer);
+                //wake buyer
+                synchronized (buyer) {
+                    buyer.notify();
+                }
+            } else {
+                Util.sleep(100);
+            }
+        }
+        System.out.println(this + " закрыл кассу");
+    }
+
+
+    @Override
+    public String toString() {
+        return "\tКассир №" + number;
+    }
+
+}
