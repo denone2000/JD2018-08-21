14d82ecdbbec52c90340186544abcaea154dff4b
Галушка Игорь <galuropek@gmail.com>
2018-09-30 14:14:22 +0300

    jd02_03 demo atomic

---
+++ src/by/it/galushka/draft/atomicDemo/AtomicIntegerDemo.java
@@ -0,0 +1,17 @@
+package by.it.galushka.draft.atomicDemo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerDemo {
+    public static void main(String[] args) {
+        AtomicInteger atomic = new AtomicInteger(0);
+        atomic.incrementAndGet();
+        System.out.println("incr: " + atomic);
+        atomic.incrementAndGet();
+        System.out.println("incr: " + atomic);
+        atomic.decrementAndGet();
+        System.out.println("decr: " + atomic);
+        int i = atomic.get();
+        System.out.println("get(): " + i);
+    }
+}
