7c21d650d2d4930c7990a364e377fa56edfb8507
Галушка Игорь <galuropek@gmail.com>
2018-09-30 11:40:26 +0300

    jd02_03 getting ready

--- src/by/it/galushka/draft/ExecutorsRun.java
+++
@@ -1,4 +0,0 @@
-package by.it.galushka.draft;
-
-public class ExecutorsRun {
-}
---
+++ src/by/it/galushka/draft/executorsDemo/ExecutorsRun.java
@@ -0,0 +1,26 @@
+package by.it.galushka.draft.executorsDemo;
+
+public class ExecutorsRun extends Thread {
+
+    ExecutorsRun(int number) {
+        super("Thread №" + number);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this + " создан.");
+        Util.sleep(1000);
+        System.out.println(this + " стартовал.");
+        Util.sleep(100);
+        System.out.println(this + " работает.");
+        Util.sleep(Util.getRandom(2000, 5000));
+        System.out.println(this + " закончил работу.");
+        Util.sleep(100);
+        System.out.println(this + " уничтожен.");
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
