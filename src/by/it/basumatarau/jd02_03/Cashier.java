789c29fbc95c6a718bd7e47143cd2608192be956
Basumatarau <basumatarau@gmail.com>
2018-10-01 08:37:05 +0300

    jd02_03. start...

---
+++ src/by/it/basumatarau/jd02_03/Cashier.java
@@ -0,0 +1,102 @@
+package by.it.basumatarau.jd02_03;
+
+import java.util.Map;
+
+public class Cashier implements Runnable {
+    private String name;
+    Cashier(int cashierNum){
+        name = "Cashier#" + cashierNum;
+        Dispatcher.addCashierOnShift();
+        Dispatcher.openCashier();
+        Dispatcher.addCashierMonitor(CASHIER_MONITOR, this);
+    }
+    private final Object CASHIER_MONITOR = new Object();
+
+    static double getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    private static double totalRevenue=0.0;
+    private boolean isWorking=true;
+    private Buyer currBuyer = null;
+
+    boolean isWorking() {
+        return isWorking;
+    }
+    Buyer getCurrBuyer(){
+        return currBuyer;
+    }
+
+    Object getCASHIER_MONITOR(){
+        return CASHIER_MONITOR;
+    }
+    @Override
+    public void run() {
+        //System.out.println(this +" has started service");
+
+        while(Dispatcher.isNotOnTarget()){
+            Buyer buyer = Buyer.pollBuyer();
+            if(buyer!=null) {
+
+                currBuyer=buyer;
+                //System.out.println(this + " is servicing " + buyer);
+                double charge = buyer.putGoodsOutOfBasket().stream().map((good) -> {
+                    Double result = 0.0;
+                    for (Map.Entry<String, Double> entry : Goods.getGoods().entrySet()) {
+                        if (entry.getKey().equals(good)) {
+                            result = entry.getValue();
+                        }
+                    }
+                    return result;
+                }).reduce(0.0, (price1, price2) -> price1 + price2);
+                //System.out.printf("%s has been charged %s$\n", buyer ,charge);
+                totalRevenue+=charge;
+
+                Util.sleep(Util.random(2000, 5000));
+                //System.out.println(this + " has finished servicing " + buyer);
+                synchronized (buyer) {
+                    buyer.notify();
+                }
+
+                /* close the cashier if(condition1&&condition2)
+                 * condition1: not enough customers in the market
+                 */
+                if(Dispatcher.getOpenCashiers()>Math.ceil((double)Dispatcher.getBuyersInMarket()/5)){
+                    synchronized (CASHIER_MONITOR){
+                        try {
+                            //System.out.println(this + " is waiting for more customers to come to the market");
+                            Dispatcher.closeCashier();
+                            isWorking=false;
+                            CASHIER_MONITOR.wait();
+                            isWorking=true;
+                            //System.out.println(this + " got back to work");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }else if(Dispatcher.getBuyersInMarket()>0){
+                synchronized (CASHIER_MONITOR){
+                    try {
+                        //System.out.println(this + " is waiting for the customers");
+                        Dispatcher.closeCashier();
+                        isWorking=false;
+                        CASHIER_MONITOR.wait();
+                        isWorking=true;
+                        //System.out.println(this + " got back to work");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        //System.out.println(this + " has done its shift");
+        Dispatcher.removeCashierOnShift();
+        isWorking=false;
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
+}
