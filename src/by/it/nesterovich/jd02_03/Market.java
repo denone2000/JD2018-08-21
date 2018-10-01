15ee5143ba001cb91e5d97ddc6c6963d480b735d
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-10-01 18:53:51 +0300

    jd02_03 variant C opening of cash desks as the queue increases (there is no closure of cash desks when the queue is reduced)

--- src/by/it/nesterovich/jd02_03/Market.java
+++ src/by/it/nesterovich/jd02_03/Market.java
@@ -1,7 +1,5 @@
 package by.it.nesterovich.jd02_03;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,10 +7,9 @@ public class Market {
 
     private static int counterBuyer = 0;
     private static int currentTime = 0;
+    private static int countCashier = 0;
 
     public static void main(String[] args) {
-
-        List<Thread> threads = new ArrayList<>();
 
         int buyersInMarket = Dispatcher.getBuyersInMarket();
 
@@ -20,27 +17,19 @@ public class Market {
 
         ExecutorService executors = Executors.newFixedThreadPool(5);
 
-        for (int i = 1; i <= 5; i++) {
-            Cashier cashier = new Cashier(i);
-            executors.execute(cashier);
-        }
-
         while (Dispatcher.marketIsOpened()) {
             if (currentTime % 60 < 30 && buyersInMarket < currentTime % 60 + 10) {
                 addBuyer();
             } else if (buyersInMarket <= 40 + (30 - (currentTime % 60))) {
                 addBuyer();
             }
-            Util.sleep(1000);
-            System.out.println("Текучее время: >>>>..... " + currentTime + " .....<<<<\tMAIN: Всего в магазине: " + Dispatcher.getBuyersInMarket());
-            currentTime++;
-        }
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if (QueueBuyers.getCount() >= 4 * countCashier + countCashier) { //проверить в этом блоке хрупкость кода
+                Cashier cashier = new Cashier(++countCashier);
+                executors.execute(cashier);
             }
+            Util.sleep(1000);
+            System.out.println("Текущее время: >>>>..... " + currentTime + " .....<<<<\tMAIN: Всего в магазине: " + Dispatcher.getBuyersInMarket());
+            currentTime++;
         }
         executors.shutdown();
         while (!executors.isTerminated()) Util.sleep(100);
@@ -54,9 +43,7 @@ public class Market {
                 Buyer buyer = new Buyer(++counterBuyer);
                 Buyer.addBuyersToList(buyer);
                 buyer.start();
-                //System.out.println("Текучее время: >>>>..... " + currentTime + " .....<<<<\tMAIN: Всего в магазине: " + Dispatcher.getBuyersInMarket());
             }
         }
     }
-
 }
