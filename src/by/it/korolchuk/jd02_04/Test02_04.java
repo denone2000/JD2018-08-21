1c3be7176eb861a4887c81d2b5dba2fea871beb8
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2018-10-04 23:32:26 +0300

    JD02_04 A сделано

--- src/by/it/korolchuk/jd02_04/Test02_04.java
+++ src/by/it/korolchuk/jd02_04/Test02_04.java
@@ -7,28 +7,28 @@ import static org.junit.Assert.*;
 public class Test02_04 {
 
     @Test
-    public void testAddCalc(){
+    public void testAdd(){
         Parser parser = new Parser();
         Var calc = parser.calc("2+2");
         assertEquals("4.0", calc.toString());
     }
 
     @Test
-    public void testSubCalc(){
+    public void testSub(){
         Parser parser = new Parser();
         Var calc = parser.calc("2-2");
         assertEquals("0.0", calc.toString());
     }
 
     @Test
-    public void testMulCalc() {
+    public void testMul() {
         Parser parser = new Parser();
         Var calc = parser.calc("2*2");
         assertEquals("4.0", calc.toString());
     }
 
     @Test
-    public void testDivCalc() {
+    public void testDiv() {
         Parser parser = new Parser();
         Var calc = parser.calc("2/2");
         assertEquals("1.0", calc.toString());
@@ -37,7 +37,9 @@ public class Test02_04 {
     @Test
     public void testExpression() {
         Parser parser = new Parser();
-        Var calc = parser.calc("A=2+2*-3/-2");
-        assertEquals("5.0", calc.toString());
+        assertEquals("7.3", parser.calc("A=2+5.3").toString());
+        assertEquals("25.55", parser.calc("B=A*3.5").toString());
+        assertEquals("25.0", parser.calc("B1=B+0.11*-5").toString());
+        assertEquals("2.65", parser.calc("B2=A/2-1").toString());
     }
 }
