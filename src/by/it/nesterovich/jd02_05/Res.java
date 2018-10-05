920da7ff1b761b782b8b43148a7d9b0505211352
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-10-05 17:42:12 +0300

    jd02_05 variant B

--- src/by/it/nesterovich/jd02_05/Res.java
+++ src/by/it/nesterovich/jd02_05/Res.java
@@ -3,7 +3,7 @@ package by.it.nesterovich.jd02_05;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public enum  Res {
+public enum Res {
 
     INSTANCE;
 
@@ -13,15 +13,11 @@ public enum  Res {
         setLocale(Locale.getDefault());
     }
 
-    void setLocale(Locale locale){
-        bundle = ResourceBundle.getBundle(
-                "by.it.nesterovich.jd02_05.res.strings", locale
-        );
+    void setLocale(Locale locale) {
+        bundle = ResourceBundle.getBundle("by.it.nesterovich.jd02_05.res.strings", locale);
     }
 
-    String get(String key){
+    String get(String key) {
         return bundle.getString(key);
     }
-
-
 }
--- src/by/it/nesterovich/jd02_05/TaskA.java
+++ src/by/it/nesterovich/jd02_05/TaskA.java
@@ -1,18 +1,33 @@
 package by.it.nesterovich.jd02_05;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class TaskA {
+
     public static void main(String[] args) {
-        Res res = Res.INSTANCE;
-        if (args.length == 2) {
-            String language = args[0];
-            String country = args[1];
-            Locale locale = new Locale(language, country);
-            res.setLocale(locale);
+
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        System.out.println("Для завершения программы введите: end");
+        System.out.println("Для переключения языка приветствие введите одну из команд: ru be en");
+        while (!(line = scanner.nextLine()).equalsIgnoreCase("end")) {
+            Locale locale = new Locale(line);
+            writeToConsole(locale);
         }
+    }
+
+    private static void writeToConsole(Locale locale) {
+        Res res = Res.INSTANCE;
+        Date now = new Date();
+        res.setLocale(locale);
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG, locale);
+
         System.out.println(res.get(Str.MSG_WELCOME));
         System.out.println(res.get(Str.MSG_QUESTION));
         System.out.println(res.get(Str.TXT_NAME));
+        System.out.println(dateFormat.format(now));
     }
 }
