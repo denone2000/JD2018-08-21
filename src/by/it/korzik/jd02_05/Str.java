255c3ec7dc015471ef91f2d1823b107b2adfb155
KorzikD <korzikdmitriy@gmail.com>
2018-10-05 18:46:58 +0300

    jd02_04

--- src/by/it/korzik/jd02_05/Str.java
+++ src/by/it/korzik/jd02_05/Str.java
@@ -1,7 +1,10 @@
 package by.it.korzik.jd02_05;
 
-public final class Str {
-    private Str(){}
-    static final String MSG_WELCOME="message.welcome";
-    static final String MSG_QUESTION="message.question";
+final class Str {
+    private Str() {
+    }
+
+    static final String MSG_WELCOME = "message.welcome";
+    static final String MSG_QUESTION = "message.question";
+    static final String MSG_NAME = "message.name";
 }
--- src/by/it/korzik/jd02_05/TaskB.java
+++ src/by/it/korzik/jd02_05/TaskB.java
@@ -3,17 +3,16 @@ package by.it.korzik.jd02_05;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
-public class TaskA {
+public class TaskB {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String lang = scanner.next();
         Res res = Res.INSTANCE;
-        if (args.length == 2) {
-            String language = args[0];
-            String country = args[1];
-            Locale locale = new Locale(language, country);
-            res.setLocale(locale);
-        }
+        Locale locale = new Locale(lang);
+        res.setLocale(locale);
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, res.getLocale());
         String date = dateFormat.format(new Date());
         System.out.println(date);
