4529ce09e8cbc4f29e4b0642a3bc9687de9503a5
KorzikD <korzikdmitriy@gmail.com>
2018-10-05 16:00:43 +0300

    jd02_04

---
+++ src/by/it/korzik/jd02_05/Str.java
@@ -0,0 +1,7 @@
+package by.it.korzik.jd02_05;
+
+public final class Str {
+    private Str(){}
+    static final String MSG_WELCOME="message.welcome";
+    static final String MSG_QUESTION="message.question";
+}
---
+++ src/by/it/korzik/jd02_05/TaskA.java
@@ -0,0 +1,9 @@
+package by.it.korzik.jd02_05;
+
+public class TaskA {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("How do you do");
+
+    }
+}
