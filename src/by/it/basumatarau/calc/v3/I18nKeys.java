d06070fdd782b1cc43fcbd0fac07c98660c44994
Basumatarau <basumatarau@gmail.com>
2018-10-05 19:27:01 +0300

    jd02_06. tasks a, b and c have been done...

---
+++ src/by/it/basumatarau/calc/v3/I18nKeys.java
@@ -0,0 +1,15 @@
+package by.it.basumatarau.calc.v3;
+
+abstract class I18nKeys {
+    static final String PARSER_EXCEPTION_WRONG_PARENTH = "ExceptionMessage.Parser.wrongParenthOrder";
+    static final String PARSER_EXCEPTION_OPERATION_NOT_IMPLEMENTED = "ExceptionMessage.Parser.operationNotImplemented";
+    static final String PARSER_EXCEPTION_UNKNOWN_OPERATOR_PRECEDENCE = "ExceptionMessage.Parser.unknownOperatorPrecedence";
+    static final String PRINTER_EXCEPTION_NULL_POINTER_INPUT = "ExceptionMessage.Printer.nullPointerInput";
+    static final String VAR_EXCEPTION_OPERAND_NOT_PARSED = "ExceptionMessage.Var.operandNotParsed";
+    static final String VAR_EXCEPTION_ILLEGAL_SUM_OPERATION = "ExceptionMessage.Var.illegalSumOperation";
+    static final String VAR_EXCEPTION_ILLEGAL_SUB_OPERATION = "ExceptionMessage.Var.illegalSubOperation";
+    static final String VAR_EXCEPTION_ILLEGAL_MUL_OPERATION = "ExceptionMessage.Var.illegalMulOperation";
+    static final String VAR_EXCEPTION_ILLEGAL_DIV_OPERATION = "ExceptionMessage.Var.illegalDivOperation";
+    static final String VAR_EXCEPTION_DIV_BY_ZERO = "ExceptionMessage.Var.divisionByZero";
+
+}
---
+++ src/by/it/basumatarau/jd02_05/Str.java
@@ -0,0 +1,7 @@
+package by.it.basumatarau.jd02_05;
+
+abstract class Str {
+    static final String MSG_WELCOME = "message.welcome";
+    static final String MSG_QUESTION = "message.question";
+    static final String MSG_NAME = "text.name";
+}
