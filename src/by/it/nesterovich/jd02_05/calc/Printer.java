0dfcebf2d29df4d53d800c71495f02186c43304e
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-10-06 07:14:12 +0300

    jd02_05 variant С

---
+++ src/by/it/nesterovich/jd02_05/calc/Printer.java
@@ -0,0 +1,10 @@
+package by.it.nesterovich.jd02_05.calc;
+
+class Printer {
+
+    void print(Var variable) {
+        if (variable != null) {
+            System.out.println(variable);
+        }
+    }
+}
