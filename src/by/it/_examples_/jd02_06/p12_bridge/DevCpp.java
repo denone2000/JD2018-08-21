a9e93ee8d3906c35238b0d4d93933f9a4c3d3be4
Khmelov <375336849110@tut.by>
2018-10-06 14:23:26 +0300

    JD02_06 fix demo

--- src/by/it/_examples_/jd02_06/p12_bridge/DevCpp.java
+++ src/by/it/_examples_/jd02_06/p12_bridge/DevCpp.java
@@ -3,6 +3,6 @@ package by.it._examples_.jd02_06.p12_bridge;
 class DevCpp implements Dev {
     @Override
     public void writeCode() {
-        System.out.println("JavaDeveloper write code...");
+        System.out.println("CPP Developer write code...");
     }
 }
