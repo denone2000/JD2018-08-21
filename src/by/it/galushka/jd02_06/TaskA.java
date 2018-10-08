8bf264f85591dae24a3e7229b33349967b9203ae
Галушка Игорь <galuropek@gmail.com>
2018-10-08 14:59:04 +0300

    jd02_06 A

---
+++ src/by/it/galushka/jd02_06/TaskA.java
@@ -0,0 +1,22 @@
+package by.it.galushka.jd02_06;
+
+public class TaskA {
+
+    private static MySingleton logger = MySingleton.getMySingleton();
+
+    public static void main(String[] args) {
+        logger.log("ERROR!!!");
+        getExeption();
+    }
+
+    private static void getExeption() {
+        String[] array = new String[3];
+        try {
+            String str = array[3];
+            System.out.println(str);
+        } catch (Exception e) {
+            String message = e.toString();
+            logger.log(message);
+        }
+    }
+}
