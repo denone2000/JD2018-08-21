88f5a0f5e679b7185bcfb47d5abe96efd8b3ecd7
Khmelov <375336849110@tut.by>
2018-10-16 00:06:02 +0300

    lesson 03_01

--- src/by/it/_examples_/jd03_01/Ex_04__Init.java
+++ src/by/it/_examples_/jd03_01/Ex_04__Init.java
@@ -26,15 +26,15 @@ public class Ex_04__Init {
                              (URL_DB, USER_DB, PASSWORD_DB);
              Statement statement = connection.createStatement()) {
             //пересоздание базы целиком
-            statement.executeUpdate("DROP SCHEMA IF EXISTS `akhmelev` ;");
-            statement.executeUpdate("CREATE SCHEMA IF NOT EXISTS `akhmelev` DEFAULT CHARACTER SET utf8 ;");
+            statement.executeUpdate("DROP SCHEMA IF EXISTS `itacademy` ;");
+            statement.executeUpdate("CREATE SCHEMA IF NOT EXISTS `itacademy` DEFAULT CHARACTER SET utf8 ;");
             //удаление всех таблиц (если работает удаление схемы то это лишнее)
-            statement.executeUpdate("DROP TABLE IF EXISTS `akhmelev`.`ads`;");
-            statement.executeUpdate("DROP TABLE IF EXISTS `akhmelev`.`users`;");
-            statement.executeUpdate("DROP TABLE IF EXISTS `akhmelev`.`roles`;");
+            statement.executeUpdate("DROP TABLE IF EXISTS `itacademy`.`ads`;");
+            statement.executeUpdate("DROP TABLE IF EXISTS `itacademy`.`users`;");
+            statement.executeUpdate("DROP TABLE IF EXISTS `itacademy`.`roles`;");
             //генерация всех таблиц
             statement.executeUpdate(
-                    "CREATE TABLE IF NOT EXISTS `akhmelev`.`roles` (\n" +
+                    "CREATE TABLE IF NOT EXISTS `itacademy`.`roles` (\n" +
                             "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
                             "  `Role` VARCHAR(45) NULL DEFAULT NULL,\n" +
                             "  PRIMARY KEY (`ID`))\n" +
@@ -43,7 +43,7 @@ public class Ex_04__Init {
                             "DEFAULT CHARACTER SET = utf8;\n"
             );
             statement.executeUpdate(
-                    "CREATE TABLE IF NOT EXISTS `akhmelev`.`users` (\n" +
+                    "CREATE TABLE IF NOT EXISTS `itacademy`.`users` (\n" +
                             "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
                             "  `Login` VARCHAR(45) NULL DEFAULT NULL,\n" +
                             "  `Password` VARCHAR(45) NULL DEFAULT NULL,\n" +
@@ -53,13 +53,13 @@ public class Ex_04__Init {
                             "  INDEX `fk_users_FK_rolesx` (`FK_roles` ASC),\n" +
                             "  CONSTRAINT `fk_users_roles`\n" +
                             "    FOREIGN KEY (`FK_roles`)\n" +
-                            "    REFERENCES `akhmelev`.`roles` (`ID`))\n" +
+                            "    REFERENCES `itacademy`.`roles` (`ID`))\n" +
                             "ENGINE = InnoDB\n" +
                             "AUTO_INCREMENT = 1\n" +
                             "DEFAULT CHARACTER SET = utf8;\n"
             );
             statement.executeUpdate(
-                    "CREATE TABLE IF NOT EXISTS `akhmelev`.`ads` (\n" +
+                    "CREATE TABLE IF NOT EXISTS `itacademy`.`ads` (\n" +
                             "  `ID` INT(11) NOT NULL AUTO_INCREMENT,\n" +
                             "  `RoomCount` INT(11) NULL DEFAULT NULL,\n" +
                             "  `Floor` INT(11) NULL DEFAULT NULL,\n" +
@@ -73,7 +73,7 @@ public class Ex_04__Init {
                             "  INDEX `fk_ads_users1_idx` (`FK_users` ASC),\n" +
                             "  CONSTRAINT `fk_ads_users1`\n" +
                             "    FOREIGN KEY (`FK_users`)\n" +
-                            "    REFERENCES `akhmelev`.`users` (`ID`)\n" +
+                            "    REFERENCES `itacademy`.`users` (`ID`)\n" +
                             "    ON DELETE CASCADE\n" +
                             "    ON UPDATE CASCADE)\n" +
                             "ENGINE = InnoDB\n" +
@@ -81,12 +81,12 @@ public class Ex_04__Init {
                             "DEFAULT CHARACTER SET = utf8;\n"
             );
             //заполнение строками
-            statement.executeUpdate("INSERT INTO `akhmelev`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Admin');\n");
-            statement.executeUpdate("INSERT INTO `akhmelev`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'User');\n");
-            statement.executeUpdate("INSERT INTO `akhmelev`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Guest');\n");
-            statement.executeUpdate("INSERT INTO `akhmelev`.`users` (`ID`, `Login`, `Password`, `Email`, `FK_roles`) VALUES (DEFAULT, 'Admin', 'admin', 'adm@it.by', 1);\n");
-            statement.executeUpdate("INSERT INTO `akhmelev`.`users` (`ID`, `Login`, `Password`, `Email`, `FK_roles`) VALUES (DEFAULT, 'User', 'user', 'user@it.by', 2);\n");
-            statement.executeUpdate("INSERT INTO `akhmelev`.`ads` (`ID`, `RoomCount`, `Floor`, `Floors`, `Price`, `Area`, `Description`, `Address`, `FK_users`) VALUES (DEFAULT, 1, 2, 5, 55555, 50, 'Тестовое объявление', 'Кнорина 9', 2);\n");
+            statement.executeUpdate("INSERT INTO `itacademy`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Admin');\n");
+            statement.executeUpdate("INSERT INTO `itacademy`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'User');\n");
+            statement.executeUpdate("INSERT INTO `itacademy`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Guest');\n");
+            statement.executeUpdate("INSERT INTO `itacademy`.`users` (`ID`, `Login`, `Password`, `Email`, `FK_roles`) VALUES (DEFAULT, 'Admin', 'admin', 'adm@it.by', 1);\n");
+            statement.executeUpdate("INSERT INTO `itacademy`.`users` (`ID`, `Login`, `Password`, `Email`, `FK_roles`) VALUES (DEFAULT, 'User', 'user', 'user@it.by', 2);\n");
+            statement.executeUpdate("INSERT INTO `itacademy`.`ads` (`ID`, `RoomCount`, `Floor`, `Floors`, `Price`, `Area`, `Description`, `Address`, `FK_users`) VALUES (DEFAULT, 1, 2, 5, 55555, 50, 'Тестовое объявление', 'Кнорина 9', 2);\n");
 
         } catch (Exception e) {
             e.printStackTrace();
