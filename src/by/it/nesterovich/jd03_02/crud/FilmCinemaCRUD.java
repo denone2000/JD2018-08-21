d2740d41d33717299c6d70fe019e31b73f197194
Aleksandr <nesterovich.aleksandr.a@gmail.com>
2018-10-19 15:21:08 +0300

    jd03_02 CRUD

---
+++ src/by/it/nesterovich/jd03_02/crud/FilmCinemaCRUD.java
@@ -0,0 +1,70 @@
+package by.it.nesterovich.jd03_02.crud;
+
+import by.it.nesterovich.jd03_02.ConnectionCreator;
+import by.it.nesterovich.jd03_02.beans.FilmCinema;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class FilmCinemaCRUD {
+
+    static boolean create(FilmCinema filmCinema) throws SQLException {
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format(
+                    "INSERT INTO `films_cinemas`(`films_id`, `cinemas_id`)" +
+                            " VALUES (%d,%d);",
+                    filmCinema.getFilmId(),
+                    filmCinema.getCinemaId());
+            return (statement.executeUpdate(sql) != 0);
+        }
+    }
+
+    static FilmCinema read(long filmId, long cinemaId) throws SQLException {
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format("SELECT * FROM `films_cinemas` WHERE `films_id`=%d AND `cinemas_id`=%d ", filmId, cinemaId);
+            ResultSet resultSet = statement.executeQuery(sql);
+            if (resultSet.next()) {
+                return new FilmCinema(
+                        resultSet.getLong("films_id"),
+                        resultSet.getLong("cinemas_id")
+                );
+            }
+            return null;
+        }
+    }
+
+    static boolean delete(FilmCinema filmCinema) throws SQLException {
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format(
+                    "DELETE FROM `films_cinemas` WHERE `films_id`=%d AND `cinemas_id`=%d",
+                    filmCinema.getFilmId(),
+                    filmCinema.getCinemaId());
+            return (statement.executeUpdate(sql) == 1);
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        FilmCinema filmCinema = new FilmCinema(3, 1);
+        if (create(filmCinema)) {
+            System.out.println("Create OK: " + filmCinema);
+        }
+        filmCinema = read(filmCinema.getFilmId(), filmCinema.getCinemaId());
+        if (filmCinema != null) {
+            System.out.println("Read OK: " + filmCinema);
+        }
+        if (delete(filmCinema)) {
+            System.out.println("Delete OK: " + filmCinema);
+        }
+    }
+}
---
+++ src/by/it/nesterovich/jd03_02/crud/ReservedTicketCRUD.java
@@ -0,0 +1,112 @@
+package by.it.nesterovich.jd03_02.crud;
+
+import by.it.nesterovich.jd03_02.ConnectionCreator;
+import by.it.nesterovich.jd03_02.beans.ReservedTicket;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ReservedTicketCRUD {
+
+    static boolean create(ReservedTicket ReservedTicket) throws SQLException {
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format(
+                    //INSERT INTO `reserved_tickets`(`id`, `code`, `cost`, `users_id`, `films_id`, `cinemas_id`)
+                    // VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6])
+                    "INSERT INTO `reserved_tickets`(`code`, `cost`, `users_id`, `films_id`, `cinemas_id`)" +
+                            " VALUES (%d,%f,%d,%d,%d);",
+                    ReservedTicket.getCode(),
+                    ReservedTicket.getCost(),
+                    ReservedTicket.getUsersId(),
+                    ReservedTicket.getFilmsId(),
+                    ReservedTicket.getCinemasId());
+            if (statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS) == 1) {
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    ReservedTicket.setId(generatedKeys.getLong(1));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static ReservedTicket read(long id) throws SQLException {
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format("SELECT * FROM `reserved_tickets` WHERE `id`=%d ", id);
+            ResultSet resultSet = statement.executeQuery(sql);
+            if (resultSet.next()) {
+                return new ReservedTicket(
+                        resultSet.getLong("id"),
+                        resultSet.getInt("code"),
+                        resultSet.getDouble("cost"),
+                        resultSet.getLong("users_id"),
+                        resultSet.getLong("films_id"),
+                        resultSet.getLong("cinemas_id")
+                );
+            }
+            return null;
+        }
+    }
+
+    static boolean update(ReservedTicket ReservedTicket) throws SQLException {
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format(
+                    "UPDATE `reserved_tickets` SET " +
+                            "`code`=%d," +
+                            "`cost`=%f," +
+                            "`users_id`=%d," +
+                            "`films_id`=%d," +
+                            "`cinemas_id`=%d" +
+                            " WHERE `id`=%d",
+                    ReservedTicket.getCode(),
+                    ReservedTicket.getCost(),
+                    ReservedTicket.getUsersId(),
+                    ReservedTicket.getFilmsId(),
+                    ReservedTicket.getCinemasId());
+            return (statement.executeUpdate(sql) == 1);
+        }
+    }
+
+    static boolean delete(ReservedTicket ReservedTicket) throws SQLException {
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format(
+                    "DELETE FROM `reserved_tickets` WHERE `id`=%d",
+                    ReservedTicket.getId());
+            return (statement.executeUpdate(sql) == 1);
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        ReservedTicket ReservedTicket = new ReservedTicket(0, 126413, 2.5, 3, 2, 2);
+        if (create(ReservedTicket)) {
+            System.out.println("Create OK: " + ReservedTicket);
+        }
+        long id = ReservedTicket.getId();
+        ReservedTicket = read(id);
+        if (ReservedTicket != null) {
+            System.out.println("Read OK: " + ReservedTicket);
+        }
+        ReservedTicket.setCinemasId(2);
+        if (update(ReservedTicket)) {
+            System.out.println("Update OK: " + ReservedTicket);
+        }
+        if (delete(ReservedTicket)) {
+            System.out.println("Delete OK: " + ReservedTicket);
+        }
+    }
+}
