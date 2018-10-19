cdded44bbbc0dd405ed28c232b5229103adbc439
aliashkov <art1699584@gmail.com>
2018-10-19 17:03:45 +0300

    Reserved

---
+++ src/by/it/artemliashkov/jd03_02/ConnectionCreater.java
@@ -0,0 +1,31 @@
+package by.it.artemliashkov.jd03_02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectionCreater {
+    String URL_DB = "jdbc:mysql://127.0.0.1:2016/mydb"+
+            "?useUnicode=true&characterEncoding=UTF-8";
+    String USER_DB = "root";
+    String PASSWORD_DB = "";
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error loading driver: " + e);
+        }
+    }
+
+    private static Connection connection;
+
+    Connection getConnection() throws SQLException {
+        if(connection==null || connection.isClosed()) {
+            System.out.println("####");
+            connection = DriverManager.getConnection(URL_DB, USER_DB, PASSWORD_DB);
+        }
+        return connection;
+    }
+}
