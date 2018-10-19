3b7038510d5a0cf08a022e7092c49e109fe4424b
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2018-10-19 23:52:01 +0300

    jd03_02 B списак пользователей

---
+++ src/by/it/korolchuk/jd03_02/TaskB.java
@@ -0,0 +1,34 @@
+package by.it.korolchuk.jd03_02;
+
+import by.it.korolchuk.jd03_02.beans.Ad;
+import by.it.korolchuk.jd03_02.beans.User;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TaskB {
+    public static void main(String[] args) throws SQLException {
+
+        getUsersList();
+    }
+
+    static String getUsersList() throws SQLException {
+        String login = null;
+        try (
+                Connection connection = ConnectionCreator.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+            String sql = String.format("SELECT * FROM `users`");
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                login = resultSet.getString("login");
+                System.out.println(login);
+            }
+            return login;
+        }
+    }
+
+}
