0b16c72b7365a4d21728d01ccac82456a4f2addf
EvgeniyBindyuk <evganius@gmail.com>
2018-10-19 15:56:45 +0300

    refresh 03_01 part2
    Signed-off-by: EvgeniyBindyuk <evganius@gmail.com>

--- src/by/it/bindyuk/jd03_01/Runner.java
+++ src/by/it/bindyuk/jd03_01/Runner.java
@@ -4,6 +4,7 @@ class Runner {
     public static void main(String[] args) {
         C_Reset.resetBase();
         C_Init.initialization();
+        A_AddRoutes.addRoutes();
         A_AddUser.addUser();
         B_AddRoles.addRoles();
         A_AddData.addData();
