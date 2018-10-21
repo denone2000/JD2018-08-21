3a286d2a94771a59cd067b332521fedb6b130e92
Khmelov <375336849110@tut.by>
2018-10-22 02:04:38 +0300

    classwork lesson 03_03 (repair)

--- src/by/it/akhmelev/jd03_03/Runner.java
+++ src/by/it/akhmelev/jd03_03/Runner.java
@@ -16,34 +16,34 @@ public class Runner {
 
         User user = new User(0, "UserDao", "UserDao", "UserDao@user.ru", 2);
         if (dao.user.create(user))
-            System.out.println("CREATE:" + dao.user.getAll());
+            System.out.println("CREATE:\n" + dao.user.getAll());
         user = dao.user.read(user.getId());
         user.setLogin("uuuuuu");
         if (dao.user.update(user))
-            System.out.println("UPDATE:" + dao.user.getAll());
+            System.out.println("UPDATE:\n" + dao.user.getAll());
         if (dao.user.delete(user))
-            System.out.println("\nDELETE:" + dao.user.getAll());
+            System.out.println("DELETE:\n" + dao.user.getAll());
 
         Role role = new Role(0, "RoleDao");
         if (dao.role.create(role))
-            System.out.println("CREATE:" + dao.role.getAll());
+            System.out.println("CREATE:\n" + dao.role.getAll());
         role = dao.role.read(role.getId());
         role.setRole("rrrrrrr");
         if (dao.role.update(role))
-            System.out.println("UPDATE:" + dao.role.getAll());
+            System.out.println("UPDATE:\n" + dao.role.getAll());
         if (dao.role.delete(role))
-            System.out.println("\nDELETE:" + dao.role.getAll());
+            System.out.println("DELETE:\n" + dao.role.getAll());
 
         Ad ad = new Ad();
         ad.setDescription("ad dao test ad dao test ad dao test ");
         ad.setUsers_id(2);
         if (dao.ad.create(ad))
-            System.out.println("CREATE:" + dao.ad.getAll());
+            System.out.println("CREATE:\n" + dao.ad.getAll());
         ad = dao.ad.read(ad.getId());
         ad.setDescription("ddddddd");
         if (dao.ad.update(ad))
-            System.out.println("UPDATE:" + dao.ad.getAll());
+            System.out.println("UPDATE:\n" + dao.ad.getAll());
         if (dao.ad.delete(ad))
-            System.out.println("\nDELETE:" + dao.ad.getAll());
+            System.out.println("DELETE:" + dao.ad.getAll());
     }
 }
