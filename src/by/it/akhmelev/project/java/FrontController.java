098c89eb497ad4d6bd0ebd84c4273cfcace55306
Khmelov <375336849110@tut.by>
2018-10-23 12:15:24 +0300

    project

--- src/by/it/akhmelev/project/java/FrontController.java
+++ src/by/it/akhmelev/project/java/FrontController.java
@@ -11,18 +11,35 @@ public class FrontController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        process(req,resp);
+        process(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        process(req,resp);
+        process(req, resp);
     }
 
 
     private void process(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Controller if live");
+        String command = req.getParameter("command");
+        String view = "/error.jsp";
+        switch (command) {
+            case "Index":
+                view = Actions.INDEX.jsp;
+                break;
+            case "Login":
+                view = Actions.LOGIN.jsp;
+                break;
+            case "Logout":
+                view = Actions.LOGOUT.jsp;
+                break;
+            case "SignUp":
+                view = Actions.SIGNUP.jsp;
+                break;
+        }
+
+        getServletContext().getRequestDispatcher(view).forward(req,resp);
     }
 }
